1ec8fcb3eb24f8e7f43187d2d3f46650546c421a
Mansur <mansuralikoroglu@gmail.com>
2020-08-20 10:13:37 +0300

    Test src index file

---
+++ test/src/index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+
+describe(
+    'src',
+    function () {
+        describe(
+            'index',
+            function () {
+                let index;
+                let expect;
+                let appStub;
+
+                before(function () {
+                    expect = chai.expect;
+                    chai.use(sinonChai);
+
+                    appStub = sinon.stub()
+                        .returns('appStub');
+
+                    index = proxyquire(
+                        '../../src/index.js',
+                        {
+                            './app': appStub
+                        }
+                    );
+                });
+
+                it(
+                    'should export 1 file',
+                    function () {
+                        const EXPORTED_FILES_COUNT = 1;
+
+                        expect(Object.keys(index).length)
+                            .to
+                            .be
+                            .equal(EXPORTED_FILES_COUNT);
+                    }
+                );
+
+                it(
+                    'should export correct file(s)',
+                    function () {
+                        expect(index.app())
+                            .to
+                            .be
+                            .equal('appStub');
+                    }
+                );
+            }
+        );
+    }
+);
